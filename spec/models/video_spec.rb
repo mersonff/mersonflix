89562d4d239905665c24dba570f08a1ba5a6eee4
Emerson Freitas <mersonsi@gmail.con>
2021-07-23 13:42:35 -0300

    Testando as validações com RSpec

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @video = Video.new(title: 'RED', description: 'Lorem Ipsum', url:'http://www.test.com/video')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@video).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    @video.title = ''
+    expect(@video).to_not be_valid
+  end
+
+  it 'is not valid without a description' do
+    @video.description = ''
+    expect(@video).to_not be_valid
+  end
+
+  it 'is not valid without a url' do
+    @video.url = ''
+    expect(@video).to_not be_valid
+  end
+
+  it 'is not valid with a invalid url' do
+    @video.url = 'aaaa.co'
+    expect(@video).to_not be_valid
+  end
 end
