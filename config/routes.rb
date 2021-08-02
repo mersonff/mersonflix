9261e51283cdd091dcb2fdffa8f212be4aa87a53
Emerson Freitas <mersonsi@gmail.com>
2021-08-02 17:32:42 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  root to: 'videos#list_free'
+  root 'videos#list_free'
 
   get '/videos/free', to: 'videos#list_free'
 
