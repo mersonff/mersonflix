ba41139c4068e74c945d56db166e4301301dfac3
Emerson Freitas <mersonsi@gmail.com>
2021-08-02 17:28:21 -0300

    Removendo rota raiz

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  get root_path, to: 'videos#index'
   get '/videos/free', to: 'videos#list_free'
 
   resources :videos do
